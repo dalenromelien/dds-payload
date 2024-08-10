5c10d045e0d71957e8d850fe7e780b9a02898f10
dromelie <dromelie@nyit.edu>
2024-08-09 23:41:55 -0400

    allowing cors wildcard

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -22,6 +22,7 @@ export default buildConfig({
   graphQL: {
     schemaOutputFile: path.resolve(__dirname, "generated-schema.graphql"),
   },
+  cors: ["*"],
   plugins: [payloadCloud()],
   db: mongooseAdapter({
     url: process.env.DATABASE_URI,
